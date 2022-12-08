a1e4f0cd24de52d661dad5b05f8a2d4a39d12918
luohaozhe <luohaozhe@gmail.com>
2022-12-08 14:17:31 +0800

    this is first commit

---
+++ assets/js/chunk-2d22e0d9.ba6ec0fe.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22e0d9"],{f9b2:function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div",[e._v(" 我的团购订单 ")])},a=[],u={name:""},r=u,s=c("2877"),i=Object(s["a"])(r,t,a,!1,null,"4c1f56ca",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d22e0d9.ba6ec0fe.js.map
